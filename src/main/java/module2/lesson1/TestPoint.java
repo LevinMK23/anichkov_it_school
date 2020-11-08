867de124796a244fd131cc968b2d4ad07aa05d81
levinmk23 <levinmk23@gmail.com>
2020-11-08 10:22:26 +0300

    Lesson 1 OOP

---
+++ src/main/java/module2/lesson1/TestPoint.java
@@ -0,0 +1,10 @@
+package module2.lesson1;
+
+public class TestPoint {
+    public static void main(String[] args) {
+        Point point = new Point(1,2);
+
+        System.out.println(point.getX());
+        System.out.println(point.getY());
+    }
+}
