d1d1663d8be578ef78c1b7788d6612228b45b27d
Михаил Левин <levinmk23@gmail.com>
2021-04-25 12:04:10 +0300

    Rest Api

--- module5/src/main/java/rest/JsonTest.java
+++ module5/src/main/java/rest/JsonTest.java
@@ -1,10 +1,20 @@
 package rest;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.google.gson.Gson;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
+import org.jsfr.json.JsonDomBuilder;
+import org.jsfr.json.JsonSurfer;
+
+import java.util.Map;
+
+import static net.minidev.json.parser.JSONParser.MODE_JSON_SIMPLE;
 
 public class JsonTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Rectangle rectangle = new Rectangle(1, 2);
 
@@ -16,5 +26,11 @@ public class JsonTest {
         Rectangle other = gson.fromJson(json, Rectangle.class);
 
         System.out.println(other);
+
+        JSONObject obj = (JSONObject) new JSONParser(MODE_JSON_SIMPLE)
+                .parse(json);
+
+        System.out.println(obj);
+
     }
 }
--- module5/src/main/java/rest/VkApiTest.java
+++ module5/src/main/java/rest/VkApiTest.java
@@ -1,4 +1,24 @@
 package rest;
 
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.UserAuthResponse;
+
 public class VkApiTest {
+
+    public static void main(String[] args) {
+        TransportClient transportClient = HttpTransportClient.getInstance();
+        VkApiClient vk = new VkApiClient(transportClient);
+
+//        UserAuthResponse authResponse = vk.oAuth()
+//                .userAuthorizationCodeFlow(APP_ID, CLIENT_SECRET, REDIRECT_URI, code)
+//                .execute();
+
+//        UserActor actor = new UserActor(authResponse.getUserId(),
+//                authResponse.getAccessToken());
+    }
 }
+
+
